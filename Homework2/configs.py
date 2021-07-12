57ffa353f75852c754901a043d3eef5b25103fa1
TYTTYTTYT <tai.yintao@gmail.com>
2021-07-12 16:00:39 +0800

    Addcross validation for CNN, add auto training codes

--- Homework2/configs.py
+++ Homework2/configs.py
@@ -2,6 +2,11 @@
 # The diretory to store the fashion MNIST data
 FASHION_ROOT = "/home/tai/XDF_Project/MIT-Reserach/Homework2/data"
 
+# Store the CNN training logs
+LOG_FILE = "/home/tai/XDF_Project/MIT-Reserach/Homework2/cnn.log"
+PLOT_DIR = "/home/tai/XDF_Project/MIT-Reserach/Homework2/plots/"
+MODEL_DIR = "/home/tai/XDF_Project/MIT-Reserach/Homework2/trained_models/"
+
 # Constants, do not modify
 # MEAN and STD are statistics of pixel-wise training set images
 MEAN = 0.286041
---
+++ Homework2/utils.py
@@ -0,0 +1,39 @@
+'''
+Please add utililty functions here
+'''
+import torch
+import matplotlib.pyplot as plt
+from configs import *
+
+def check_accuracy(loader, model, device):
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device)
+            y = y.to(device=device)
+            model.to(device)
+
+            predictions = model.predict(x)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+    model.train()
+    return num_correct/num_samples
+
+def plot_valid_train_accu(steps, val_accur, train_accur, title):
+    fig, (ax1) = plt.subplots(1, 1)
+    ax1.plot(steps, val_accur, label='Validation')
+    ax1.plot(steps, train_accur, label='Training')
+
+    ax1.set_xlabel('Training steps')
+    ax1.set_ylabel('Accuracy')
+
+    ax1.legend()
+
+    fig.suptitle(title)
+
+    plt.savefig(PLOT_DIR + title +'.png')
+
